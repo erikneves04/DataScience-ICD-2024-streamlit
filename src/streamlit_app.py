d713ab3a83dcbb1dd0f46db26b4b312004d6d53a
Erik Neves <erikrrn04@gmail.com>
2024-08-08 16:39:36 -0300

    teste barras

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -26,6 +26,25 @@ PlotTitle(selected)
 if selected == "Análise Exploratória":
   PlotJustifyText("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
   
+  with st.expander("Análise 1: Distribuição dos Dados"):
+    st.write("Aqui você pode incluir gráficos e análises relacionados à distribuição dos dados.")
+    # Exemplos de visualizações
+    st.line_chart([1, 2, 3, 4])
+    st.bar_chart([1, 2, 3, 4])
+
+  with st.expander("Análise 2: Correlações"):
+      st.write("Aqui você pode incluir gráficos e análises relacionadas às correlações entre variáveis.")
+      # Exemplo de visualização
+      st.area_chart([1, 2, 3, 4])
+
+  with st.expander("Análise 3: Análise de Outliers"):
+      st.write("Aqui você pode incluir gráficos e análises relacionadas à identificação e tratamento de outliers.")
+      # Exemplo de visualização
+      st.dataframe({
+          'Coluna 1': [1, 2, 3, 4],
+          'Coluna 2': [4, 3, 2, 1]
+      })
+
 elif selected == "Testes de Hipótese":
   PlotJustifyText("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
 

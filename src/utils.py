d40c1ed2b71894e3548eb40194d084100146bc01
Erik Neves <erikrrn04@gmail.com>
2024-08-08 17:15:48 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,6 @@ def PlotGraph(plt):
     st.pyplot(plt)
 
 def LoadDatabases():
-    kindle_data = pd.read_csv('../databases/kindle_data_clean.csv')
-    books = pd.read_csv('../databases/goodreads_basic_info_2.csv')
+    kindle_data = pd.read_csv('databases/kindle_data_clean.csv')
+    books = pd.read_csv('databases/goodreads_basic_info_2.csv')
     return kindle_data, books

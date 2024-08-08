c8fea575ed13d7d460bb717593fa9e88241efd04
Erik Neves <erikrrn04@gmail.com>
2024-08-08 17:51:34 -0300

    add RC1 e RC2

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -22,13 +22,18 @@ PlotTitle(selected)
 
 if selected == "Análise Exploratória":
   PlotJustifyText("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
-  
   for topic in ExploratoryAnalysisTopics():
       with st.expander(topic[0]):
           topic[1]()
 
 elif selected == "Testes de Hipótese":
   PlotJustifyText("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
+  for topic in HypothesisTestsTopics():
+      with st.expander(topic[0]):
+          topic[1]()
 
 elif selected == "Regressão e Classificação":
   PlotJustifyText("Nesse notebook temos como objetivo aplicar modelos de regressão para prever valores numéricos contínuos e modelos de classificação para categorizar dados em classes distintas. E, apartir dessa análise, extrair informações dos conjuntos de dados e hipóteses abordados previamente.")
+  for topic in RegressionClassificationTopics():
+      with st.expander(topic[0]):
+          topic[1]()

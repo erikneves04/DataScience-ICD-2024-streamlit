46d5576bc4b5150a579067438487a9e2fc95b316
Erik Neves <erikrrn04@gmail.com>
2024-08-08 17:12:29 -0300

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
-from utils import PlotTitle, PlotJustifyText
+import utils
 from Exploratory_Analysis import Topics as ExploratoryAnalysisTopics
 from Hypothesis_Tests import Topics as HypothesisTestsTopics
 from Regression_And_Classification import Topics as RegressionClassificationTopics
@@ -18,17 +18,17 @@ with st.sidebar:
 
   st.header('Introdução a Ciência de Dados(ICD-2024/01) \n Desenvolvido por: \n - Erik Neves \n - Gabriel Prudente \n - Vitor Costa \n - Philipe')
 
-PlotTitle(selected)
+utils.PlotTitle(selected)
 
 if selected == "Análise Exploratória":
-  PlotJustifyText("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
+  utils.PlotJustifyText("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
   
   for topic in ExploratoryAnalysisTopics():
       with st.expander(topic[0]):
           topic[1]()
 
 elif selected == "Testes de Hipótese":
-  PlotJustifyText("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
+  utils.PlotJustifyText("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
 
 elif selected == "Regressão e Classificação":
-  PlotJustifyText("Nesse notebook temos como objetivo aplicar modelos de regressão para prever valores numéricos contínuos e modelos de classificação para categorizar dados em classes distintas. E, apartir dessa análise, extrair informações dos conjuntos de dados e hipóteses abordados previamente.")
+  utils.PlotJustifyText("Nesse notebook temos como objetivo aplicar modelos de regressão para prever valores numéricos contínuos e modelos de classificação para categorizar dados em classes distintas. E, apartir dessa análise, extrair informações dos conjuntos de dados e hipóteses abordados previamente.")

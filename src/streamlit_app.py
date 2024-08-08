8e92ae4ad1dea88b7e4f2f7d31db9c8ddfaae427
Erik Neves <erikrrn04@gmail.com>
2024-08-08 16:23:53 -0300

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -7,31 +7,26 @@ from streamlit_option_menu import option_menu
 
 with st.sidebar:
   selected = option_menu(
-    menu_title = "Main Menu",
-    options = ["Home","Projects","Contact"],
+    menu_title = "Menus Principal",
+    options = ["Análise Exploratória","Testes de Hipótese","Regressão e Classificação"],
     icons = ["house","book","envelope"],
-    menu_icon = "cast",
+    menu_icon = "book",
     default_index = 0,
 
   )
-  if selected == "Home":
+
+  st.header('Introdução a Ciência de Dados(ICD-2024). \n Desenvolvido por: \n - Erik Neves \n - Gabriel Prudente \n - Vitor Costa \n - Philipe')
+
+  if selected == "Análise Exploratória":
     st.title(f"You Have selected {selected}")
-    st.header('Snowflake Healthcare App')
-    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-    my_cur = my_cnx.cursor()
-    # run a snowflake query and put it all in a var called my_catalog
-    my_cur.execute("select * from SWEATSUITS")
-    my_catalog = my_cur.fetchall()
-    st.dataframe(my_catalog)
-    q1 = st.text_input('Write your query','')
-    st.button('Run Query')
-    if not q1:
-      st.error('Please write a query')
-    else:
-      my_cur.execute(q1)
-      my_catalog = my_cur.fetchall()
-      st.dataframe(my_catalog)
-  if selected == "Projects":
-    st.title(f"You Have selected {selected}")
-  if selected == "Contact":
-    st.title(f"You Have selected {selected}")
+    st.write("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
+  
+  
+  if selected == "Testes de Hipótese":
+    st.title(f"Testes de Hipótese {selected}")
+    st.write("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
+  
+  
+  if selected == "Rergressão e classificação":
+    st.title(f"Regressão e classificação {selected}")
+    st.write("Nesse notebook temos como objetivo aplicar modelos de regressão para prever valores numéricos contínuos e modelos de classificação para categorizar dados em classes distintas. E, apartir dessa análise, extrair informações dos conjuntos de dados e hipóteses abordados previamente.")

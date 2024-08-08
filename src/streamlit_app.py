05dfc9ac341711508373203ad5e6f3638d2515ea
Erik Neves <erikrrn04@gmail.com>
2024-08-08 16:32:13 -0300

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,6 +4,12 @@ import snowflake.connector
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
+def PlotTitle(title: str):
+  st.markdown(f"<h1 style='text-align: center; color: black;'>{title}</h1>", unsafe_allow_html=True)
+
+def PlotJustifyText(text: str):
+  st.markdown(f"<p style='text-align: justify; color: black;'>{text}</p>", unsafe_allow_html=True)
+
 with st.sidebar:
   selected = option_menu(
     menu_title="Menus Principal",
@@ -15,29 +21,13 @@ with st.sidebar:
 
   st.header('Introdução a Ciência de Dados(ICD-2024). \n Desenvolvido por: \n - Erik Neves \n - Gabriel Prudente \n - Vitor Costa \n - Philipe')
 
-# Conteúdo do lado direito
+PlotTitle(selected)
+
 if selected == "Análise Exploratória":
-  st.title(selected)
-  st.write("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
+  PlotJustifyText("Nesta fase, realizamos uma análise exploratória dos dados, visualizando diferentes aspectos das informações coletadas. Utilizamos diversos gráficos para identificar padrões, tendências e possíveis relações entre variáveis. Essa etapa é crucial para obter insights preliminares e orientar as próximas fases da análise, garantindo uma compreensão mais profunda dos dados antes da aplicação de modelos mais complexos.")
   
 elif selected == "Testes de Hipótese":
-  st.title(f"Testes de Hipótese {selected}")
-  
-  col1, col2 = st.columns([2, 3])
-  
-  with col1:
-    st.write("Conteúdo na coluna da esquerda para testes de hipótese...")
-  
-  with col2:
-    st.write("Conteúdo na coluna da direita para testes de hipótese...")
+  PlotJustifyText("Nesta seção, realizamos testes de hipóteses para investigar as relações entre variáveis específicas no conjunto de dados. Utilizamos testes estatísticos para verificar se as diferenças observadas entre grupos são estatisticamente significativas ou se podem ser atribuídas ao acaso.")
 
 elif selected == "Regressão e Classificação":
-  st.title(f"Regressão e classificação {selected}")
-  
-  col1, col2 = st.columns([2, 3])
-  
-  with col1:
-    st.write("Conteúdo na coluna da esquerda para regressão e classificação...")
-  
-  with col2:
-    st.write("Conteúdo na coluna da direita para regressão e classificação...")
+  PlotJustifyText("Nesse notebook temos como objetivo aplicar modelos de regressão para prever valores numéricos contínuos e modelos de classificação para categorizar dados em classes distintas. E, apartir dessa análise, extrair informações dos conjuntos de dados e hipóteses abordados previamente.")

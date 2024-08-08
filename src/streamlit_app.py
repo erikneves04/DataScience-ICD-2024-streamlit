374afc36cc002e143a5d4bc668b612cf962bfa0d
Erik Neves <erikrrn04@gmail.com>
2024-08-08 16:33:56 -0300

    fix color

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,10 +5,10 @@ import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
 def PlotTitle(title: str):
-  st.markdown(f"<h1 style='text-align: center; color: black;'>{title}</h1>", unsafe_allow_html=True)
+  st.markdown(f"<h1 style='text-align: center; color: white'>{title}</h1>", unsafe_allow_html=True)
 
 def PlotJustifyText(text: str):
-  st.markdown(f"<p style='text-align: justify; color: black;'>{text}</p>", unsafe_allow_html=True)
+  st.markdown(f"<p style='text-align: justify; color: white;'>{text}</p>", unsafe_allow_html=True)
 
 with st.sidebar:
   selected = option_menu(
@@ -19,7 +19,7 @@ with st.sidebar:
     default_index=0,
   )
 
-  st.header('Introdução a Ciência de Dados(ICD-2024). \n Desenvolvido por: \n - Erik Neves \n - Gabriel Prudente \n - Vitor Costa \n - Philipe')
+  st.header('Introdução a Ciência de Dados(ICD-2024/01) \n Desenvolvido por: \n - Erik Neves \n - Gabriel Prudente \n - Vitor Costa \n - Philipe')
 
 PlotTitle(selected)
 
